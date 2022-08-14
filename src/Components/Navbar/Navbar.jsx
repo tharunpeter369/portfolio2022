8e8834f8ddec25693f0088b0020dca7c0c70678b
Tharun <tharun.petere@lanwaresolutions.com>
2022-08-14 19:40:20 +0530

    navbar almost completed

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="n-wrapper">
+        <div className="n-left">
+            <div className="n-name">Tharun</div>
+            <span>toggle</span>
+        </div>
+        <div className="n-right">
+            <div className="n-list">
+                <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Experiance</li>
+                    <li>Portfolio</li>
+                    <li>Testimonial</li>
+                </ul>
+            </div>
+            <button className="button n-button">
+                contact us
+            </button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
