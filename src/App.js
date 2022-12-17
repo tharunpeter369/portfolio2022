3f3dbaf85f57c4df47737fc302236183e686f774
tharun <tharunpeter369@gmail.com>
2022-12-17 23:11:05 +0530

    will finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Navbar from "./Components/Navbar/Navbar";
 import './App.css';
 import Intro from "./Components/intro/Intro";
+import Services from "./Components/Services/Services";
+import Experience from "./Components/Experience/Experience";
+import Works from "./Components/Works/Works";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonials from "./Components/Testimonials/Testimonials";
 
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Intro/>
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Works />
+      <Portfolio />
+      <Testimonials/>
     </div>
   );
 }
